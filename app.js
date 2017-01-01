03b539f947664c95e3feaf792d47249562fc6f04
Bennie <brobinson88@gmail.com>
2017-01-01 16:48:59 -0500

    Consequence of Sound B+, Stereogum Heavy Rotation, filter unique by album, consistent formatting of artist and album

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const xml2js      = require('xml2js');
 const concat      = require('concat-stream');
 const jsdom       = require('jsdom');
 const ProgressBar = require('progress');
+const _           = require('lodash');
 const jsonfile    = require('jsonfile');
 const parser      = new xml2js.Parser();
 parser.on('err', console.error);
@@ -12,18 +13,18 @@ const getXmlParse = (url, title) =>
     http.get(url, response => {
       response.on('error', err => rej(err));
 
-      const len = parseInt(response.headers['content-length'], 10);
+      // const len = parseInt(response.headers['content-length'], 10);
 
-      const bar = new ProgressBar('   downloading ' + title + ' [:bar] :percent :etas', {
-        complete: '=',
-        incomplete: ' ',
-        width: 20,
-        total: len
-      });
+      // const bar = new ProgressBar('   downloading ' + title + ' [:bar] :percent :etas', {
+      //   complete: '=',
+      //   incomplete: ' ',
+      //   width: 20,
+      //   total: len
+      // });
 
-      response.on('data', function (chunk) {
-        bar.tick(chunk.length);
-      });
+      // response.on('data', function (chunk) {
+      //   bar.tick(chunk.length);
+      // });
 
       response.pipe(concat(buffer => {
         const str = buffer.toString();
@@ -55,15 +56,20 @@ const writeJson = (fileName, data) =>
     })
   );
 
-const today         = new Date(Date.now());
-const items         = xmlObj => xmlObj.rss.channel[0].item;
-const getUrlPromise = item => getjQueryDom(item.link[0]);
-const getPubDate    = item => new Date(Date.parse(item.pubDate));
-const dateString    = pubDate => pubDate.toDateString().slice(4);
+const today              = new Date(Date.now());
+const items              = xmlObj => xmlObj.rss.channel[0].item;
+const getUrlPromise      = item => getjQueryDom(item.link[0]);
+const getPubDate         = item => new Date(Date.parse(item.pubDate));
+const dateString         = pubDate => pubDate.toDateString().slice(4);
+const toTitleCase        = str => str.replace(/\w\S*/g, txt =>
+  txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+const removeSpecialChars = str => str.replace(/[^\w\s]/gi, '');
+const cleanUp            = str => toTitleCase(removeSpecialChars(str));
 
 // The final collection of albums
 let allAlbums = [];
 
+// Collector where need to fetch scores
 const buildReviewCollector = (url, title, reducer) =>
   getXmlParse(url, title)
     .then(xmlObj => {
@@ -78,20 +84,33 @@ const buildReviewCollector = (url, title, reducer) =>
     },
     error => console.error(error));
 
+// Collector where no need to fetch scores
+const buildCollector = (url, title, reducer) =>
+  getXmlParse(url, title)
+    .then(xmlObj => {
+      const collection = items(xmlObj);
+
+      return Promise.resolve(collection)
+        .then(reducer)
+        .then(albums => {
+          allAlbums = allAlbums.concat(albums);
+        });
+    },
+    console.error);
+
 const metacriticReducer = (results, collection) =>
   results.reduce((acc, html, i) => {
     const score   = parseInt(html.$('[itemprop=ratingValue]').html());
     const pubDate = getPubDate(collection[i]);
 
-    if (score > 80 && today.getMonth() === pubDate.getMonth()) {
+    if (score > 80
+      && today.getMonth() === pubDate.getMonth()
+    ) {
       const [album, artist] = collection[i].title[0].split(' by ');
       return acc.concat({
-        artist: artist,
-        album: album,
-        date: dateString(pubDate),
-        scores: {
-          metacritic: score
-        }
+        artist: cleanUp(artist),
+        album: cleanUp(album),
+        date: dateString(pubDate)
       });
     }
 
@@ -103,15 +122,51 @@ const pitchforkReducer = (results, collection) =>
     const score   = parseFloat(html.$('.score').html());
     const pubDate = getPubDate(collection[i]);
 
-    if (score > 7.4 && today.getMonth() === pubDate.getMonth()) {
+    if (score > 8.0
+      && today.getMonth() === pubDate.getMonth()
+    ) {
       const [artist, album] = collection[i].title[0].split(': ');
       return acc.concat({
-        artist: artist,
-        album: album,
-        date: dateString(pubDate),
-        scores: {
-          pitchfork: score
-        }
+        artist: cleanUp(artist),
+        album: cleanUp(album),
+        date: dateString(pubDate)
+      });
+    }
+
+    return acc;
+  }, []);
+
+const cosReducer = (results, items) =>
+  results.reduce((acc, html, i) => {
+    const acceptedScale = ['B+', 'A-', 'A', 'A+'];
+    const score = html.$('.grade-badge').html();
+    const pubDate = getPubDate(items[i]);
+
+    if (acceptedScale.includes(score)
+      && today.getMonth() === pubDate.getMonth()
+    ) {
+      const [_, artistAlbum] = items[i].title[0].split(': ');
+      const [artist, album] = artistAlbum.split(' – ');
+      return acc.concat({
+        artist: cleanUp(artist),
+        album: cleanUp(album),
+        date: dateString(pubDate)
+      });
+    }
+
+    return acc;
+  }, []);
+
+const stereogumReucer = (items) =>
+  items.reduce((acc, item) => {
+    const pubDate = getPubDate(item);
+
+    if (today.getMonth() === pubDate.getMonth()) {
+      const [artist, album] = item.title[0].split(' – ');
+      return acc.concat({
+        artist: cleanUp(artist),
+        album: cleanUp(album),
+        date: dateString(pubDate)
       });
     }
 
@@ -132,10 +187,26 @@ const getPitchforkReviews = () =>
     pitchforkReducer
   );
 
+const getCosReviews = () =>
+  buildReviewCollector(
+    'http://consequenceofsound.net/category/reviews/album-reviews/feed/',
+    'consequence of sound',
+    cosReducer
+  );
+
+const getStereogumReviews = () =>
+  buildCollector(
+    'http://www.stereogum.com/heavy-rotation/feed/',
+    'stereogum',
+    stereogumReucer
+  );
+
 Promise.all([
   getPitchforkReviews(),
-  getMetacriticReviews()
+  getMetacriticReviews(),
+  getCosReviews(),
+  getStereogumReviews()
 ])
 .then(() =>
-  writeJson('./albums.json', {albums: allAlbums}))
+  writeJson('./albums.json', {albums: _.uniqBy(allAlbums, 'album')}))
 .then(res => console.log(res));
