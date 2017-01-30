0a7d515b7c652695224505623e77574e05a4b2d5
Bennie <brobinson88@gmail.com>
2017-01-30 10:01:08 -0500

    Add sidebar for monthly view

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/january', function(req, res) {
+  res.sendFile(path.join(__dirname + '/jan.html'));
+});
+
 app.listen(process.env.PORT || 8080);
