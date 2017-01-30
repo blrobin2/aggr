534726029fec7e09b10d0aa2c97b40daf5254ab8
Bennie <brobinson88@gmail.com>
2017-01-30 10:38:11 -0500

    Refactor static html to simple templating

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const path        = require('path');
 const express     = require('express');
 const app         = express();
+const fs          = require('fs');
 
 app.use(express.static('public'));
 
-// Routes
+// Templating
+app.engine('ntl', function (filePath, options, callback) {
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(err);
+    const rendered = content.toString()
+      .replace('#title#', options.title)
+      .replace('#json#', options.json);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+    return callback(null, rendered);
+  });
 });
 
-app.get('/january', function(req, res) {
-  res.sendFile(path.join(__dirname + '/jan.html'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ntl');
+
+// Routes
+app.get('/', function (req, res) {
+  res.render('list', {
+    title: '',
+    json: 'albums.json'
+  });
+});
+
+app.get('/january', function (req, res) {
+  res.render('list', {
+    title: 'January',
+    json: 'jan.json'
+  });
 });
 
 app.listen(process.env.PORT || 8080);
