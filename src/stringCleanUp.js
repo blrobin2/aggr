9e408dccb3ef7b4dbf5175039bb847e643950c16
Bennie <brobinson88@gmail.com>
2018-03-19 14:31:02 -0400

    Add â to escaped characters

--- src/stringCleanUp.js
+++ src/stringCleanUp.js
@@ -3,9 +3,9 @@ const toTitleCase = str =>
     /\w\S*/g,
     txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   );
-const umlatReplacements = { ä: "a", ü: "u", ö: "o", ß: "ss" };
+const umlatReplacements = { ä: "a",	â: "a", ü: "u", ö: "o", ß: "ss" };
 const replaceUmlats = str =>
-  str.replace(/[äöüß]/g, match => umlatReplacements[match]);
+  str.replace(/[äâöüß]/g, match => umlatReplacements[match]);
 const removeSpecialChars = str =>
   str.replace(/[^\w\s&amp;]/gi, "").replace(/\B&amp;\B/gi, "&");
 
